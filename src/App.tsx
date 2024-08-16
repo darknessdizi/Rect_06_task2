67c8bf897b8057e697c9fb6af1cf1ff62ec712b6
Remezov Dmitriy <darknessdizi@gmail.com>
2024-08-16 22:55:07 +0700

    добавлены стили и разметка страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,42 @@
 import { useState } from 'react';
 import './App.css';
-// import { Clock } from './components/Clock/Clock';
-// import { Form } from './components/Form/Form';
-// import { IAppState } from './modal/modal';
 
 function App() {
-  // const [formValue, setFormValue] = useState<IAppState>({
-  //   title: '',
-  //   zone: '',
-  //   arrayClock: [],
-  // })
-
-  // const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
-  //   // Нажали кнопку добавить в нашей форме
-  //   event.preventDefault();
-  //   const obj = {
-  //     title: formValue.title,
-  //     zone: formValue.zone,
-  //   };
-
-  //   setFormValue({
-  //     ...formValue,
-  //     title: '',
-  //     zone: '',
-  //     arrayClock: [...formValue.arrayClock, obj],
-  //   });
-  // }
-
-  // const changeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   // Обрабатываем изменение в поле input
-  //   const {name, value} = event.target;
-
-  //   if (name === 'zone') {
-  //     setFormValue({
-  //       ...formValue,
-  //       [name]: value.trim(),
-  //     });
-  //   }
-
-  //   if (name === 'title') {
-  //     setFormValue({
-  //       ...formValue,
-  //       [name]: value,
-  //     });
-  //   }
-  // }
-
-  // const clickCross = (event: React.ChangeEvent<HTMLDivElement>) => {
-  //   // Нажали кнопку удаления часов
-  //   const parent = event.target.closest('.clock__item');
-  //   if (parent) {
-  //     const clockTitle = parent.querySelector('.clock__title');
-  //     if (clockTitle) {
-  //       const valueTitle = clockTitle.textContent;
-  //       let array = [...formValue.arrayClock];
-  //       array = array.filter((item) => item.title != valueTitle);
-  //       setFormValue({
-  //         ...formValue,
-  //         arrayClock: [...array],
-  //       });
-  //     }
-  //   }
-  // }
+  
 
   return (
     <div className="conteiner">
 
-      {/* <div className="conteiner__form">
-        <Form title={formValue.title} zone={formValue.zone} submit={handleSubmit} change={changeInput} ></Form>
+      <div className="conteiner__header">
+        <h1 className="header__title">Notes</h1>
+        <div className="btn__update"></div>
       </div>
-      
 
-      <div className="conteiner__clock">
-        <Clock list={formValue.arrayClock} callback={clickCross} />
-      </div> */}
+      <div className="conteiner__form">
+        <form className="form__content">
+          <label className="c">
+            <span className="label__content">New Note</span>
+            <textarea className="form__textarea" name="text" rows="10" required ></textarea>
+          </label>
+          <button type="submit" className="form__btn"></button>
+        </form>
+      </div>
+
+      <div className="conteiner__cards">
+        <div className="cards__item">
+          <div className="item__context">
+            <p>много текста много текста много текста много текста много текста много текста</p>
+          </div>
+          <button type="button" className="cards__item__btn"></button>
+        </div>
+
+        <div className="cards__item">
+          <div className="item__context">
+            <p>много текста много текста много текста много текста много текста много текста много текста много текста много текста много текста много текста много текстамного текста много текста много текста много текста много текста много текста</p>
+          </div>
+          <button type="button" className="cards__item__btn"></button>
+        </div>
+      </div>
 
     </div>
   )
